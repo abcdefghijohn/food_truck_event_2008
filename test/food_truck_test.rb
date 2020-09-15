df0b794306bc4b75317cc085ba41d054d89564ae
John Kim <abcdefghijohnk@gmail.com>
2020-09-15 13:21:23 -0600

    can add stock test pass

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -9,6 +9,18 @@ class FoodTruckTest <  Minitest::Test
 
     assert_instance_of FoodTruck, food_truck
     assert_equal "Rocky Mountain Pies", food_truck.name
-    assert_equal ({}), food_truck.inventory 
+    assert_equal ({}), food_truck.inventory
+  end
+
+  def test_it_can_add_stock
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+    assert_equal 0, food_truck.check_stock(item1)
+    food_truck.stock(item1, 30)
+
+    expected = {item1 => 30}
+    assert_equal expected, food_truck.inventory
+    assert_equal 30, food_truck.check_stock(item1)
   end
 end
