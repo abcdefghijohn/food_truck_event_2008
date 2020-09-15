a20bfaf165715d361829e14d4eefa245f1d64d85
John Kim <abcdefghijohnk@gmail.com>
2020-09-15 13:36:42 -0600

    can get food truck names test pass

--- lib/event.rb
+++ lib/event.rb
@@ -9,4 +9,11 @@ class Event
   def add_food_truck(food_truck)
     @food_trucks << food_truck
   end
+
+  def food_truck_names
+    truck_names = @food_trucks.map do |truck|
+      truck.name
+    end
+    truck_names
+  end
 end
